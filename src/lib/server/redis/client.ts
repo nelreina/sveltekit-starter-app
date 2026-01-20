1548e636d80defdc37a74a2fbf56e3b81aadde09
Nelson Reina (@nelreina) <nelreina@gmail.com>
2026-01-20 03:15:35 -0400

    building to prevent trying to connect to redis while runing docker build

--- src/lib/server/redis/client.ts
+++ src/lib/server/redis/client.ts
@@ -1,7 +1,9 @@
 import { RedisClient } from 'bun';
 import { REDIS_URL } from '$env/static/private';
+import { building } from '$app/environment';
 
 const client = new RedisClient(REDIS_URL);
-await client.connect();
+
+if (!building) await client.connect();
 
 export { client as redis };
